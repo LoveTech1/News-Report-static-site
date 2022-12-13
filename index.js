01247e549d4fb7db67fe6d0a630e3f4a8254c86a
Bibek <bibekrider321@gmail.com>
2022-12-13 22:49:38 +0545

    added more functionality

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const facebookHover = document.querySelector('.fb');
-const discordHover = document.querySelector('.dis');
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
 
+toggleBtn.addEventListener("click", function() {
+    // if (sidebar.classList.contains("show-sidebar")) {
+    //   sidebar.classList.remove("show-sidebar");
+    // } else {
+    //   sidebar.classList.add("show-sidebar");
+    // }
+    sidebar.classList.toggle("show-sidebar");
+});
 
-facebookHover.addEventListener('mouseover', (event) => {
-    event.src = "/images/bluefacebook.png"
-})
-
-discordHover.addEventListener('mouseover', (event) => {
-    event.src = '/images/bluediscord.png';
-})
+closeBtn.addEventListener("click", function() {
+    sidebar.classList.remove("show-sidebar");
+});
